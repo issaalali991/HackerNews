82694e1f94f2e13b8078fcb6df43bd7189849c3c
Bepixeled <git@bepixeled.dev>
2024-01-02 15:25:15 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='text-4xl font-bold'>Let's get creative</div>
+       
+    </>
+  )
+}
+
+export default App
